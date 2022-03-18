d866e08917ac85ce0df054a746798fa17a9bd006
praefervidus <e.fleming1234@gmail.com>
2022-03-17 19:43:30 -0700

    clean up tests

--- apps/demo/src/app/app.component.spec.ts
+++ apps/demo/src/app/app.component.spec.ts
@@ -1,16 +1,25 @@
+import { of } from 'rxjs';
 import { Shallow } from 'shallow-render';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 import { LoggingService } from './logging/logging.service';
 import { SilentLogger } from './logging/silent-logger';
+import { MazesService } from './mazes/mazes.service';
 
+const mockMazesService = {
+  getMazes: jest.fn(() => of([])),
+  putMaze: jest.fn((maze) => of())
+};
 
 describe('AppComponent', () => {
   let component: Shallow<AppComponent>;
 
   beforeEach(() => {
     component = new Shallow(AppComponent, AppModule)
-      .provideMock({ provide: LoggingService, useClass: SilentLogger });
+      .provideMock(
+        { provide: LoggingService, useClass: SilentLogger },
+        { provide: MazesService, useValue: mockMazesService },
+      );
     jest.clearAllMocks();
   });
 
@@ -27,5 +36,10 @@ describe('AppComponent', () => {
   it('should render a banner message', async () => {
     const { find } = await component.render();
     expect(find('h1').nativeElement.textContent).toBe('Welcome to Valant demo');
+  });
+
+  it('gets existing mazes from API on init', async () => {
+    await component.render();
+    expect(mockMazesService.getMazes).toBeCalledTimes(1);
   });
 });
